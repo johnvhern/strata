ef849b3eeade2f240f22eca0915305fb91bc9be7
John Vhern T. Dela Cruz <johnvhern.dc@gmail.com>
2026-04-30 10:07:16 +0800

    Added additional dropdowns placeholder

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Strata.Controllers
+{
+    [Authorize]
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
